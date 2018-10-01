9064e91b17aa19a19048b7d16ae86c21e9b12789
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-10-01 16:24:53 -0400

    scrapped old sidebar, working on new one
    started over on side bar navigation

--- js/portfolio/navbar.js
+++ js/portfolio/navbar.js
@@ -1,25 +1,15 @@
-function navControl(){
-	if (document.getElementById('navBar').style.width == "40px") {
+function navControl(x){
+	if (document.getElementById('navBar').style.width == "0px") {
+		x.classList.toggle("change");
 		document.getElementById("main").style.marginLeft = "180px";
 		document.getElementById("navBar").style.width = "180px";
-		document.getElementById("menuButtonTable").style.paddingRight = "10px";
-		$("#menuButton").animate({"opacity": 0}, 300, function() {$(this).html("X").animate({"opacity": 1}, 300);});
-		$("#home").animate({"opacity": 0}, 300, function() {$(this).html("<a href=\"#\" class=\"navBarLink\">Home</a>").animate({"opacity": 1}, 300);});
-		$("#about").animate({"opacity": 0}, 300, function() {$(this).html("<a href=\"#\" class=\"navBarLink\">About</a>").animate({"opacity": 1}, 300);});
-		$("#work").animate({"opacity": 0}, 300, function() {$(this).html("<a href=\"#\" class=\"navBarLink\">Work</a>").animate({"opacity": 1}, 300);});
-		$("#contact").animate({"opacity": 0}, 300, function() {$(this).html("<a href=\"#\" class=\"navBarLink\">Contact</a>").animate({"opacity": 1}, 300);});
-		$("#firstName").animate({"opacity": 0}, 300, function() {$(this).html("JOSEPH").animate({"opacity": 1}, 300);});
-		$("#lastName").animate({"opacity": 0}, 300, function() {$(this).html("MARTIN").animate({"opacity": 1}, 300);});
+		document.getElementById('menuButton').style.left = "195px";
+		$("#home, #about, #work, #contact, #firstName, #lastName").fadeTo(800,1);
 	} else {
-		document.getElementById("main").style.marginLeft = "40px";
-		document.getElementById("navBar").style.width = "40px";
-		document.getElementById("menuButtonTable").style.paddingRight = "4px";
-		$("#menuButton").animate({"opacity": 0}, 300, function() {$(this).html("&#9776").animate({"opacity": 1}, 300);});
-		$("#home").animate({"opacity": 0}, 300, function() {$(this).html("<img class=\"navBarIcon\" src=\"images\/home.png\" width=\"30\" height=\"30\">").animate({"opacity": 1}, 300);});
-		$("#about").animate({"opacity": 0}, 300, function() {$(this).html("<img class=\"navBarIcon\" src=\"images\/about.png\" width=\"30\" height=\"30\">").animate({"opacity": 1}, 300);});
-		$("#work").animate({"opacity": 0}, 300, function() {$(this).html("<img class=\"navBarIcon\" src=\"images\/work.png\" width=\"30\" height=\"30\">").animate({"opacity": 1}, 300);});
-		$("#contact").animate({"opacity": 0}, 300, function() {$(this).html("<img class=\"navBarIcon\" src=\"images\/contact.png\" width=\"30\" height=\"30\">").animate({"opacity": 1}, 300);});
-		$("#firstName").animate({"opacity": 0}, 300, function() {$(this).html("J").animate({"opacity": 1}, 300);});
-		$("#lastName").animate({"opacity": 0}, 300, function() {$(this).html("M").animate({"opacity": 1}, 300);});
+		x.classList.toggle("change");
+		$("#home, #about, #work, #contact, #firstName, #lastName").fadeTo(400,0.1);
+		document.getElementById("main").style.marginLeft = "0px";
+		document.getElementById("navBar").style.width = "0px";
+		document.getElementById('menuButton').style.left = "15px";
 	}
 }
