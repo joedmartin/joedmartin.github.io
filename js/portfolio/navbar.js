cf36a5798bb5e072ad60a6e4ca77950a78c69f56
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-10-03 11:59:12 -0400

    continued working on new navbar
    navbar is mostly done, hamburger menu button effect now works but needs to be smaller, also need to change all values to relative ones such as %/em/rem

--- js/portfolio/navbar.js
+++ js/portfolio/navbar.js
@@ -1,15 +1,15 @@
 function navControl(x){
-	if (document.getElementById('navBar').style.width == "0px") {
+	if (document.getElementById('navBar').style.width == "180px") {
 		x.classList.toggle("change");
-		document.getElementById("main").style.marginLeft = "180px";
-		document.getElementById("navBar").style.width = "180px";
-		document.getElementById('menuButton').style.left = "195px";
-		$("#home, #about, #work, #contact, #firstName, #lastName").fadeTo(800,1);
-	} else {
-		x.classList.toggle("change");
-		$("#home, #about, #work, #contact, #firstName, #lastName").fadeTo(400,0.1);
+		$("#home, #about, #work, #contact, #firstName, #lastName").animate({opacity: 0}, 400);
 		document.getElementById("main").style.marginLeft = "0px";
 		document.getElementById("navBar").style.width = "0px";
 		document.getElementById('menuButton').style.left = "15px";
+	} else {
+		x.classList.toggle("change");
+		$("#home, #about, #work, #contact, #firstName, #lastName").animate({opacity: 1}, 800);
+		document.getElementById("main").style.marginLeft = "180px";
+		document.getElementById("navBar").style.width = "180px";
+		document.getElementById('menuButton').style.left = "195px";
 	}
 }
