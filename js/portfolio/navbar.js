2eb53ec77302ddec2cb9ae41417a2cb2855862bc
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-10-04 00:29:20 -0400

    cleaned up navbar code

--- js/portfolio/navbar.js
+++ js/portfolio/navbar.js
@@ -1,13 +1,13 @@
-function navControl(x){
+function navControl(menuButton){
 	if (document.getElementById('navBar').style.width == "180px") {
-		x.classList.toggle("change");
-		$("#home, #about, #work, #contact, #firstName, #lastName").animate({opacity: 0}, 400);
+		menuButton.classList.toggle("change");
+		$("#navBarContent").animate({opacity : 0},400);
 		document.getElementById("main").style.marginLeft = "0px";
 		document.getElementById("navBar").style.width = "0px";
 		document.getElementById('menuButton').style.left = "15px";
 	} else {
-		x.classList.toggle("change");
-		$("#home, #about, #work, #contact, #firstName, #lastName").animate({opacity: 1}, 800);
+		menuButton.classList.toggle("change");
+		$("#navBarContent").animate({opacity : 1},800);
 		document.getElementById("main").style.marginLeft = "180px";
 		document.getElementById("navBar").style.width = "180px";
 		document.getElementById('menuButton').style.left = "195px";
