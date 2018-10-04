d584847fb055a24135ecfb6d9c5b9ebe45a8e1fb
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-10-04 16:36:52 -0400

    navbar relative sizing
    changed the navbar to use % sizes instead of pixel values

--- js/portfolio/navbar.js
+++ js/portfolio/navbar.js
@@ -1,15 +1,15 @@
 function navControl(menuButton){
-	if (document.getElementById('navBar').style.width == "180px") {
+	if (document.getElementById('navBar').style.width == "14%") {
 		menuButton.classList.toggle("change");
 		$("#navBarContent").animate({opacity : 0},400);
-		document.getElementById("main").style.marginLeft = "0px";
-		document.getElementById("navBar").style.width = "0px";
-		document.getElementById('menuButton').style.left = "15px";
+		document.getElementById("main").style.marginLeft = "0%";
+		document.getElementById("navBar").style.width = "0%";
+		document.getElementById('menuButton').style.left = "2%";
 	} else {
 		menuButton.classList.toggle("change");
 		$("#navBarContent").animate({opacity : 1},800);
-		document.getElementById("main").style.marginLeft = "180px";
-		document.getElementById("navBar").style.width = "180px";
-		document.getElementById('menuButton').style.left = "195px";
+		document.getElementById("main").style.marginLeft = "14%";
+		document.getElementById("navBar").style.width = "14%";
+		document.getElementById('menuButton').style.left = "16%";
 	}
 }
