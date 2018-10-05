d32d5712b3441eecbbeb6c18bf70b291e9c8214f
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-10-04 21:09:57 -0400

    menu button touch screen fix
    due to hover effects sticking when using touchscreens, disabled them for the menu button when touch is detected, still work like normal when using a mouse

--- js/portfolio/navbar.js
+++ js/portfolio/navbar.js
@@ -13,3 +13,8 @@ function navControl(){
 		document.getElementById('menuButtonContainer').style.left = "195px";
 	}
 }
+
+document.addEventListener('touchstart', function addtouchclass(e){ // first time user touches the screen
+    document.documentElement.classList.add('can-touch') // add "can-touch" class to document root using classList API
+    document.removeEventListener('touchstart', addtouchclass, false) // de-register touchstart event
+}, false)
