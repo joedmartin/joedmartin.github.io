8483e1b845f7deec6db7da034301de7da6cb8886
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-10-05 10:44:31 -0400

    Started work on main header
    added image and text, still need to have text resize (maybe using media queries if there isn't another way to have text scale to page size) and add partial border around name

--- js/portfolio/navbar.js
+++ js/portfolio/navbar.js
@@ -2,13 +2,15 @@ function navControl(){
 	if (document.getElementById('navBar').style.width == "180px") {
 		$("#menuButton").animate({"opacity": 0}, 300, function() {$(this).html("&#9776").animate({"opacity": 1}, 300);});
 		$("#navBarContent").animate({opacity : 0},400);
-		document.getElementById("main").style.marginLeft = "0px";
+		//document.getElementById("main").style.marginLeft = "0px";
+		document.getElementById('header').style.opacity = "1";
 		document.getElementById("navBar").style.width = "0px";
 		document.getElementById('menuButtonContainer').style.left = "15px";
 	} else {
 		$("#menuButton").animate({"opacity": 0}, 400, function() {$(this).html("X").animate({"opacity": 1}, 400);});
 		$("#navBarContent").animate({opacity : 1},800);
-		document.getElementById("main").style.marginLeft = "180px";
+		//document.getElementById("main").style.marginLeft = "180px";
+		document.getElementById('header').style.opacity = "0.2";
 		document.getElementById("navBar").style.width = "180px";
 		document.getElementById('menuButtonContainer').style.left = "195px";
 	}
@@ -18,3 +20,5 @@ document.addEventListener('touchstart', function addtouchclass(e){ // first time
     document.documentElement.classList.add('can-touch') // add "can-touch" class to document root using classList API
     document.removeEventListener('touchstart', addtouchclass, false) // de-register touchstart event
 }, false)
+
+
