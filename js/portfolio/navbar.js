7213014e72ed4e5b830a4c2859d5a9042ac115fe
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-10-08 11:21:59 -0400

    navbar rework
    reworked navbar to use tables so spacing/alignment is easier, still need to fix jitter that occurs on side of nameplate during jquery animation

--- js/portfolio/navbar.js
+++ js/portfolio/navbar.js
@@ -1,24 +1,22 @@
 function navControl(){
 	if (document.getElementById('navBar').style.width == "180px") {
 		$("#menuButton").animate({"opacity": 0}, 300, function() {$(this).html("&#9776").animate({"opacity": 1}, 300);});
-		$(".navBarContent").animate({opacity : 0},400);
-		//document.getElementById("main").style.marginLeft = "0px";
-		document.getElementById('header').style.opacity = "1";
+		$("#navBarTable").animate({opacity : 0},300);
+		$("#header").animate({opacity : 1},300);
 		document.getElementById("navBar").style.width = "0px";
 		document.getElementById('menuButtonContainer').style.left = "15px";
 	} else {
-		$("#menuButton").animate({"opacity": 0}, 400, function() {$(this).html("X").animate({"opacity": 1}, 400);});
-		$(".navBarContent").animate({opacity : 1},800);
-		//document.getElementById("main").style.marginLeft = "180px";
-		document.getElementById('header').style.opacity = "0.2";
+		$("#menuButton").animate({"opacity": 0}, 300, function() {$(this).html("X").animate({"opacity": 1}, 300);});
+		$("#navBarTable").animate({opacity : 1},800);
+		$("#header").animate({opacity : 0.2},300);
 		document.getElementById("navBar").style.width = "180px";
 		document.getElementById('menuButtonContainer').style.left = "195px";
 	}
 }
 
-document.addEventListener('touchstart', function addtouchclass(e){ // first time user touches the screen
-    document.documentElement.classList.add('can-touch') // add "can-touch" class to document root using classList API
-    document.removeEventListener('touchstart', addtouchclass, false) // de-register touchstart event
+document.addEventListener('touchstart', function addtouchclass(e){
+    document.documentElement.classList.add('can-touch')
+    document.removeEventListener('touchstart', addtouchclass, false)
 }, false)
 
 
