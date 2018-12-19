b7c404651408325bb9f776aff28b66c2d3b7ccbd
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-12-19 16:43:09 -0500

    added flashing  colon for clock
    newtabpage

--- js/newtabpage/newtabpage.js
+++ js/newtabpage/newtabpage.js
@@ -8,32 +8,41 @@ $( document ).ready(function() {
 });
 
 // Function for getting the time and date data and updating every second.
+var toggle = true;
 function updateTimeAndDate() {
-	// Create a new date object.
-	var timeAndDate = new Date();
-	// Reference arrays for the days of the week and months of the year.
-	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	// Sets the date by getting the literal day of the week, month, numeric date, and year from the date object.
-	// Get the current time in hours and minutes from the date object. The "getTime()" function converts from 24 to 12 hour format as well as adds the AM/PM suffixes.
-	document.getElementById("time").innerHTML = getTime(timeAndDate.getHours(), timeAndDate.getMinutes().toString());
-	document.getElementById("date").innerHTML = days[timeAndDate.getDay()] + ", " + months[timeAndDate.getMonth()] + " " + timeAndDate.getDate();
+	$('#colon').animate({opacity: toggle?"-=1":"+=1"}, 500);
+	if(toggle == true) {
+		// Create a new date object.
+		var timeAndDate = new Date();
+		// Reference arrays for the days of the week and months of the year.
+		var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		// Sets the date by getting the literal day of the week, month, numeric date, and year from the date object.
+		// Get the current time in hours and minutes from the date object. The "getTime()" function converts from 24 to 12 hour format as well as adds the AM/PM suffixes.
+		document.getElementById("hour").innerHTML = formatHours(timeAndDate.getHours());
+		document.getElementById("minute").innerHTML = formatMinutes(timeAndDate.getHours(), timeAndDate.getMinutes().toString());
+		document.getElementById("date").innerHTML = days[timeAndDate.getDay()] + ", " + months[timeAndDate.getMonth()] + " " + timeAndDate.getDate();
+	}
+	toggle=!toggle;
 	// Sets a timeout so that the function will be called and the time/date updated every second.
-	setTimeout(updateTimeAndDate, 1000);
+	setTimeout(updateTimeAndDate, 500);
 }
 
-// Converts from 24 to 12 hour time and adds the AM/PM suffix accordingly.
-function getTime(hours, minutes) {
-	while (minutes.length < 2) {minutes = "0" + minutes;}
+function formatHours(hours) {
 	if (hours > 12) {
-		hours = hours - 12;
-		return (hours.toString() + ":" +  minutes + " PM");
+		return hours - 12;
 	} else if (hours == 0) {
-		return ("12:" + minutes + " AM");
+		return "12";
 	} else {
-		return (hours.toString() + ":" + minutes + " AM");
+		return hours.toString();
 	}
 }
 
-// Initial calls to the "fetchWeather()" and "updateTimeAndDate()" functions.
-//fetchWeather();
+function formatMinutes(hours, minutes) {
+	minutes = minutes.padStart(2, '0')
+	if (hours > 12) {
+		return (minutes + " PM");
+	} else {
+		return (minutes + " AM");
+	}
+}
