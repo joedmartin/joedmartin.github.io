34f3119da615c219a651b1311a75a3774208f1b6
jdm399 <32495837+jdm399@users.noreply.github.com>
2018-12-21 15:33:57 -0500

    newtabpage
    fixed bookmark icon sizes, added loading transition to page, added hover effect to bookmarks

--- js/newtabpage/newtabpage.js
+++ js/newtabpage/newtabpage.js
@@ -8,8 +8,8 @@ $( document ).ready(function() {
 	$('#welcome1').animate({marginTop:"70px",opacity:'1'},{queue:false,duration:800});
 	$('#clock').animate({bottom:"20px",opacity:'1'},{queue:false,duration:1000});
 	$('#weather').animate({bottom:"20px",opacity:'1'},{queue:false,duration:1000});
-	$('#welcome2').animate({opacity:'1'},{queue:false,duration:800});
-	$('#bookmarkContainer').animate({opacity:'1'},{queue:false,duration:800});
+	$('#welcome2').animate({opacity:'1'},{queue:false,duration:1200});
+	$('#bookmarkContainer').animate({opacity:'1'},{queue:false,duration:1200});
 	updateTimeAndDate();
 	fetchWeather();
 });
